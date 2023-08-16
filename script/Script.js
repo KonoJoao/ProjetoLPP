2084edb5b41ec165fafed4e0ea12099ba904b82a
João Vitor Alves dos Reis <joaovitoralves2009@hotmail.com>
2023-08-16 00:45:28 -0300

    javascript e bugs na view

--- script/Script.js
+++ script/Script.js
@@ -3,6 +3,26 @@ const numeroPokemon = document.querySelector(".numeroPokemon");
 const imagemPokemon = document.querySelector(".imagemDoPokemon");
 const formularioNomePokemon = document.querySelector(".formulario");
 const entrada = document.querySelector(".input_search");
+const slotsPokemon = [document.querySelector(".pokemon1"), document.querySelector(".pokemon2"), document.querySelector(".pokemon3"), document.querySelector(".pokemon4"), document.querySelector(".pokemon5"), document.querySelector(".pokemon6")]
+
+var arrayPokemons = [];
+var i=0;
+
+const adicionarPokemon = async () => {
+    var dadosPoke = await pegarDadosPokemon(nomePokemon.innerHTML);
+    console.log("deu");
+    if(arrayPokemons.length%6 == 0 && arrayPokemons.length != 0){
+    arrayPokemons.splice(i, 1, dadosPoke);
+    adicionarPokemonNaBarra(dadosPoke);
+    i++;
+    } else {
+        arrayPokemons.push(dadosPoke);
+        adicionarPokemonNaBarra(dadosPoke);
+    }
+    if(i%6==0)
+        i=0;
+
+}
 
 const pegarDadosPokemon = async (pokemon) => {
     const respostaAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -23,6 +43,17 @@ const adicionarPokemonNaTela = async (pokemon) => {
         imagemPokemon.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     else
         imagemPokemon.src = dados['sprites']['front_default'];
+    
+}
+
+const adicionarPokemonNaBarra = async (pokemon) => {
+    const dados = await pokemon;
+
+    if(dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] != null)
+    slotsPokemon[i].src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    else
+    slotsPokemon[i].src = dados['sprites']['front_default'];
+
     
 }
 
